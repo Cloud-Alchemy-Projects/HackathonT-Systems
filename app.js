dadb77d9536e3b1ab5c6140d44b8cf2ed66f954d
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-05-25 11:26:17 -0500

    First files for the server

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.status(200).send('<h1>Listo</h1>');
+})
+
+// Server configuration
+const PORT = 3000;
+app.set('port', PORT);
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.json());
+
+app.listen(app.get('port'), () =>{
+    console.log(`Server running at port ${PORT}`);
+})
