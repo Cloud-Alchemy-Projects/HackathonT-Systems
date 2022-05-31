9b3febd4e93acf735d90e48c941febc572aff9a3
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-05-30 20:00:47 -0500

    Ajustes de style.css

--- public/js/section1.js
+++ public/js/section1.js
@@ -1,12 +1,8 @@
-var hamburguesaEnMenu = document.getElementById("toggle-menu")
 var hamburguesaEnNav = document.getElementById("toggle-menu-nav")
 var menu = document.getElementById("menu-principal")
 
-hamburguesaEnMenu.addEventListener("click", ()=>{
-    menu.classList.add('hide')
-})
 
 hamburguesaEnNav.addEventListener("click", ()=>{
-    menu.classList.remove('hide')
+    menu.classList.toggle('hide')
 })
 
