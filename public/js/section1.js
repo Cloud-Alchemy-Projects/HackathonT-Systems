da80361b432f7fd2aa8dbb58cdb465097b6196f2
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-05-31 03:39:34 -0500

    Ajustes Finales

--- public/js/section1.js
+++ public/js/section1.js
@@ -1,8 +1,7 @@
 var hamburguesaEnNav = document.getElementById("toggle-menu-nav")
 var menu = document.getElementById("menu-principal")
 
-
 hamburguesaEnNav.addEventListener("click", ()=>{
-    menu.classList.toggle('hide')
+    menu.classList.toggle('active')
 })
 
