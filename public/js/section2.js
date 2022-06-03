cfc95738742c9eb459dfe3e3c9cf8127d72725a4
CrunchyBytes <alexander.diazrz@udlap.mx>
2022-06-03 03:07:28 -0500

    Opaca flechas de la Sección 2 correctamente

--- public/js/section2.js
+++ public/js/section2.js
@@ -8,47 +8,96 @@ var anterior = 0
 var numeroDeTablas = tables.length
 
 
-
+/**
+ * Muestra la tabla derecha a la actual
+ * @param {HTMLElement} flechaDerecha La flecha a la derecha de la Sección 2 (i.e., Innovative and Talented Minds)
+ */
 function deplazarDerecha(flechaDerecha) {
     flechaDerecha.addEventListener("click", () =>{
-        // if (numeroActual==0) {
-        //     flechaIzquierda.classList.remove("flecha-opaca")
-        // } else {
-        //     flechaIzquierda.classList.add("flecha-opaca")
 
-        // }
-        if (numeroActual<numeroDeTablas-1) {
+        /* Independientemente de en qué tabla se presione la flecha
+           derecha, el usuario tiene la opción para regresar a la izquierda.
+
+           Por ende, la flecha izquierda se habilita.
+         */
+        flechaIzquierda.classList.remove("flecha-opaca");
+        flechaIzquierda.classList.add("flecha");
+
+        /**
+         * La última tabla antes de que la flecha derecha se opaque
+         */
+        const LIMITE_SUPERIOR = numeroDeTablas - 2;
+
+        if (numeroActual<LIMITE_SUPERIOR) {
             anterior = numeroActual
             numeroActual ++
-            x(numeroActual, anterior, tables)
-        } 
-    })
-}
-function deplazarIzquierda(flechaIzquierda) {
-    flechaIzquierda.addEventListener("click", () =>{
-        // if (numeroActual>=numeroDeTablas-1) {
-        //     flechaDerecha.classList.remove("flecha-opaca")
-        // } else {
-        //     flechaDerecha.classList.add("flecha-opaca")
-
-        // }
-        if (numeroActual>0) {   
+            mostrarTabla(numeroActual, anterior, tables)
+            flechaDerecha.classList.remove("flecha-opaca");
+            flechaDerecha.classList.add("flecha");
+        }
+        else if(numeroActual === LIMITE_SUPERIOR)
+        {
             anterior = numeroActual
-            numeroActual --
-            x(numeroActual, anterior, tables)
+            numeroActual ++
+            mostrarTabla(numeroActual, anterior, tables)
+            flechaDerecha.classList.remove("flecha");
+            flechaDerecha.classList.add("flecha-opaca");
         }
     })
 }
 
+/**
+ * Muestra la tabla izquierda a la actual
+ * @param {HTMLElement} flechaIzquierda La flecha a la izquierda de la Sección 2 (i.e., Innovative and Talented Minds)
+ */
+function deplazarIzquierda(flechaIzquierda) {
+    flechaIzquierda.addEventListener("click", () =>{
 
-function x(numeroActual, anterior, tables)
+        /* Independientemente de en qué tabla se presione la flecha
+           izquierda, el usuario tiene la opción para regresar a la derecha.
+
+           Por ende, la flecha derecha se habilita.
+         */
+        flechaDerecha.classList.remove("flecha-opaca");
+        flechaDerecha.classList.add("flecha");
+        
+        /**
+         * La última tabla antes de que la flecha izquierda se opaque
+         */
+        const LIMITE_INFERIOR = 1;
+
+        if (numeroActual>LIMITE_INFERIOR) {
+            anterior = numeroActual
+            numeroActual --
+            mostrarTabla(numeroActual, anterior, tables)
+            flechaIzquierda.classList.remove("flecha-opaca");
+            flechaIzquierda.classList.add("flecha");
+        }
+        else if(numeroActual === LIMITE_INFERIOR)
+        {
+            anterior = numeroActual;
+            numeroActual--;
+            mostrarTabla(numeroActual, anterior, tables);
+            flechaIzquierda.classList.remove("flecha");
+            flechaIzquierda.classList.add("flecha-opaca");
+        }
+    })
+}
+
+/**
+ * Oculta la tabla anterior y muestra la tabla actual
+ * Asimismo, selecciona y deselecciona los botones correspondientes
+ * @param {Number} numeroActual El índice de la tabla actual
+ * @param {Number} anterior El índice de la tabla previa al desplace
+ * @param {HTMLElement[]} tables La colección de tablas de la Sección 2 (i.e., Innovative and Talented Minds)
+ */
+function mostrarTabla(numeroActual, anterior, tables)
 {
-    // Añadir clase "hide" a todas las tablas
     tables[anterior].classList.toggle("hide")
     tables[numeroActual].classList.toggle("hide")
-    boton[numeroActual].classList.toggle("selected")
-    boton[anterior].classList.toggle("selected")
 
+    boton[anterior].classList.toggle("selected")
+    boton[numeroActual].classList.toggle("selected")
 }
 
 deplazarDerecha(flechaDerecha)
